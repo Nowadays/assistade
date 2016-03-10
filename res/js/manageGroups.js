772ba7562bfffe474f69e4a95a52b0b75ee84ef8
Nowadays <herespieceofmine@gmail.com>
2016-03-10 18:07:52 +0100

    correction de beug + ajout de beug a corriger sur la suppression des groupes

--- res/js/manageGroups.js
+++ res/js/manageGroups.js
@@ -2,7 +2,7 @@
  * Created by Morgan on 10/03/2016.
  */
 var reg = new RegExp('^(http:\/\/[a-zA-Z0-9._\/-]*\/index.php\/)[a-zA-Z0-9._\/-]*$');
-var url = reg.exec(document.URL)[1] + 'admin/ajaxRequestSubject';
+var url = reg.exec(document.URL)[1] + 'admin/ajaxRequestGroups';
 
 var cellNames = ['Identifiant TD','Identifiant TP','Année'];
 var cellDatabaseNames = ['id_grouptd','id_grouptp','promo_id'];
